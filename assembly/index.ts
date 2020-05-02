a371df7bfb9cf370dbcb50e6984ef24c136d69c4
Feavy <quentinfo31@gmail.com>
2020-05-03 01:47:16 +0200

    Added borders

--- assembly/index.ts
+++ assembly/index.ts
@@ -22,10 +22,12 @@ export function stopMoving():void {
 
 physics.addBody(player);
 
-physics.addBody(new physics.Body2D(0, 590, 600, 10, true));
+physics.addBody(new physics.Body2D(0, 590, 900, 10, true));
 physics.addBody(new physics.Body2D(0, 300, 75, 10, true));
 physics.addBody(new physics.Body2D(200, 400, 230, 10, true));
 physics.addBody(new physics.Body2D(100, 500, 30, 10, true));
+physics.addBody(new physics.Body2D(890, 0, 10, 600, true));
+physics.addBody(new physics.Body2D(0, 0, 10, 600, true));
 
 export function update(delta:i32):void {
   physics.step();
